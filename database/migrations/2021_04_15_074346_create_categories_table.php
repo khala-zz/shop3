86a0218be2d5b5816a3d1cf8f905c18b47b214fe
dokhala <dokhaclam@gmail.com>
2021-06-05 19:13:36 +0700

    categories table

--- database/migrations/2021_04_15_074346_create_categories_table.php
+++ database/migrations/2021_04_15_074346_create_categories_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 class CreateCategoriesTable extends Migration
 {
     /**
@@ -17,11 +18,14 @@ class CreateCategoriesTable extends Migration
             $table->id();
             $table->string('title');
             $table->text('description')->nullable();
-            $table->bigInteger('parent_id')->unsigned()->nullable();
+            $table->unsignedBigInteger('parent_id')->nullable();
             $table->tinyInteger('featured')->default(0);
             $table->timestamps();
             $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
         });
+
+        
+
     }
 
     /**
