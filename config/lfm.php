6e33c0e331532e80b46a4eeb4974d1768713243d
khala-zz <dokhaclam@gmail.com>
2021-06-10 18:44:04 +0700

    Update lfm.php

--- config/lfm.php
+++ config/lfm.php
@@ -61,7 +61,7 @@ return [
             ],
         ],
         'image' => [
-            'folder_name'  => 'photos',
+            'folder_name'  => '1iuso5O6fepnoViK679d9EplkVHmN-UvY',
             'startup_view' => 'grid',
             'max_size'     => 50000, // size in KB
             'valid_mime'   => [
