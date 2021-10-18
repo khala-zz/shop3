6c490d7624d62235541339d5986854e9ac05e04f
dokhala <dokhaclam@gmail.com>
2021-10-18 12:08:19 +0700

    sua setting 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         Paginator::useBootstrap();
         Schema::defaultStringLength(191);
