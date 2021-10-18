112c4a311453b8b2dfa36ac2607021010cef76d2
dokhala <dokhaclam@gmail.com>
2021-10-18 12:00:56 +0700

    sua setting 2

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -63,7 +63,7 @@ class ProductController extends Controller
 
 
     	//get setting
-		$settings = $this -> setting -> select('setting_key','setting_value') -> where('is_active',1) ->whereNotIn('id',[4,7])->get();
+		$settings = $this -> setting -> select('id','setting_key','setting_value') -> where('is_active',1) ->whereNotIn('id',[4,7])->get();
 		dd($settings);
     	//get setting chinh sách
 		$setting_chinh_sach = $this -> setting -> select('setting_key','setting_value') -> where('is_active',1) -> where('id',1) -> first();
